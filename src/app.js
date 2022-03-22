a3248ed3ae028bd9fc0fa9bf79769ce0dfa6e0b5
Tristan <Tristandavis37@yahoo.ca>
2022-03-21 22:20:49 -0700

    wrote darkMode JS function that applies darkmode to  index.html

--- src/app.js
+++ src/app.js
@@ -1,4 +1,41 @@
-function selElement() {
-    const selEle = document.querySelector("#side-nav")
-    console.log(selEle)
+//part 2
+
+function selElement(sirod) {
+    const selEle = document.querySelector(sirod)
+    return selEle
 }
+
+console.log(selElement('nav'))
+
+//part 3
+
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+  ]
+ 
+function scholarship(stuList) {
+    const studentsScholarship = []  
+    for (var i of stuList) {
+        i.grades = i.grades.reduce((a,b) => a + b, 0) / i.grades.length; {
+            if (i.grades >= 80) {
+                i.gradeAvg = i.grades;
+                delete i.grades;
+                studentsScholarship.push(i)
+            }
+        }
+    }
+    return studentsScholarship
+}
+console.log(scholarship(students))
+
+//part 4
+
+function darkMode() {
+    const dark = document.querySelector('div')
+    dark.classList.add('dark-theme')
+}
+
+console.log(darkMode())
